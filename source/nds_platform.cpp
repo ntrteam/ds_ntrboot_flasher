cdb5719ae3dc1e615c27a4d879af6eb776948500
Sunguk Lee <d3m3vilurr@gmail.com>
2017-10-19 00:19:57 +0900

    Change libnds's APIs

--- source/nds_platform.cpp
+++ source/nds_platform.cpp
@@ -67,7 +67,7 @@ void platform::showProgress(uint32_t curr, uint32_t total, const char *status_st
 }
 
 void platform::ioDelay(unsigned long delay) {
-    ioDelay(delay);
+    swiDelay(delay);
 }
 
 int32_t platform::resetCard() {
@@ -125,22 +125,17 @@ int platform::logMessage(log_priority priority, const char *fmt, ...) {
 }
 #endif
 
-const uint8_t dummyCommand[8] = {0x9F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-const uint8_t chipIDCommand[8] = {0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-
 void reset() {
     uint8_t *garbage = (uint8_t*)malloc(sizeof(uint8_t) * 0x2000);
     if (!garbage) {
         // FIXME
         return;
     }
-    _sendCommand(dummyCommand, 0x2000, garbage, 32);
+    cardReadHeader(garbage);
     free(garbage);
 }
 
 uint32_t getChipID() {
-    uint32_t chipID;
     reset();
-    _sendCommand(chipIDCommand, 4, (uint8_t*)&chipID, 32);
-    return chipID;
+    return cardReadID(32);
 }
